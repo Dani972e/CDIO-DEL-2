4e13e5f6fa3d412d23dc920d910b779e33945f1a
Peter <peterxp@hotmail.fr>
2016-10-18 12:23:42 +0200

    Test 3 turns

--- 40_del2/src/test/testFieldController.java
+++ 40_del2/src/test/testFieldController.java
@@ -8,14 +8,26 @@ public class testFieldController {
 	public static void main(String[] args){
 		Player p1=new Player("Player1");
 		Player p2=new Player("Player2");
-		int[]player1roll={2,6};
-		int[]player2roll={2,1};
+		int[]player1roll={0,0};
+		int[]player2roll={0,0};
 		
 		FieldController.createArea(p1,p2);
 		
+		// Turn 1
+		
+		player1roll[0]=2;player1roll[1]=6;player2roll[0]=1;player2roll[1]=2;
+		
 		FieldController.play(p1,p2,player1roll,player2roll);
 		
+		// Turn 2
+		
 		player1roll[0]=6;player1roll[1]=6;player2roll[0]=2;player2roll[1]=3;
+		
+		FieldController.play(p1,p2,player1roll,player2roll);
+		
+		// Turn 3
+		
+		player1roll[0]=5;player1roll[1]=6;player2roll[0]=3;player2roll[1]=3;
 		
 		FieldController.play(p1,p2,player1roll,player2roll);
 	}

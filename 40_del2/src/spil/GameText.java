e61c414be5838d4afe635d4d33b3421c9c4083e2
Iyyel <iyyelsec@gmail.com>
2016-10-18 12:55:55 +0200

    GameText, guiDriver changes.

--- 40_del2/src/spil/GameText.java
+++ 40_del2/src/spil/GameText.java
@@ -10,4 +10,15 @@ package spil;
 
 public class GameText {
 
+	public static String[] field2Text = { "2", "Tower", "You enter the Tower of Hope and find 250 coins." };
+	public static String[] field3Text = { "3", "Crater", "You fall into the Goblin Crater. The goblins steal 100 coins from you." };
+	public static String[] field4Text = { "4", "Palace Gates", "You enter the King's Palace, and he gives you 100 coins." };
+	public static String[] field5Text = { "5", "Cold Desert", "You accidentally drop 20 coins in the desert." };
+	public static String[] field6Text = { "6", "Walled City", "It's your lucky day, 180 extra coins magically appear in your pocket." };
+	public static String[] field7Text = { "7", "Monastery", "It's time for some praying." };
+	public static String[] field8Text = { "8", "Black Cave", "You got robbed by trolls and lost 70 coins." };
+	public static String[] field9Text = { "9", "Huts in the mountain", "You find 60 coins in the hut." };
+	public static String[] field10Text = { "10", "The Werewall", "While being chased by a werewolf, you drop 80 coins." };
+	public static String[] field11Text = { "11", "The Pit", "You got knocked unconscious and lost 50 coins." };
+	public static String[] field12Text = { "12", "Goldmine", "You find a mine full of gold, worth 650 coins." };
 }
--- 40_del2/src/spil/guiDriver.java
+++ 40_del2/src/spil/guiDriver.java
@@ -10,22 +10,44 @@ public class guiDriver {
 
 	public static void main(String[] args) {
 
-		Field[] felter = {
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("2").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("3").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("4").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("5").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("6").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("7").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("8").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("9").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("10").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("11").setSubText("subtext").setDescription("desc").build(),
-				new Street.Builder().setBgColor(Color.WHITE).setFgColor(Color.BLACK).setTitle("12").setSubText("subtext").setDescription("desc").build()
+		spil.Field[] fields = {
+				new spil.Field(2, new Color(0x46B50A), new Color(0xFFFFFF), GameText.field2Text[0], GameText.field2Text[1], GameText.field2Text[2], 250),
+				new spil.Field(3, new Color(0xBA8F71), new Color(0x000000), GameText.field3Text[0], GameText.field3Text[1], GameText.field3Text[2], -100),
+				new spil.Field(4, new Color(0xAF0EA7), new Color(0x68DBFF), GameText.field4Text[0], GameText.field4Text[1], GameText.field4Text[2], 100),
+				new spil.Field(5, new Color(0x89BEFF), new Color(0x000000), GameText.field5Text[0], GameText.field5Text[1], GameText.field5Text[2], -20),
+				new spil.Field(6, new Color(0xF4E242), new Color(0x1000FF), GameText.field6Text[0], GameText.field6Text[1], GameText.field6Text[2], 180),
+				new spil.Field(7, new Color(0xFFF6B7), new Color(0x00B5AF), GameText.field7Text[0], GameText.field7Text[1], GameText.field7Text[2], 0),
+				new spil.Field(8, new Color(0x877E79), new Color(0x820707), GameText.field8Text[0], GameText.field8Text[1], GameText.field8Text[2], -70),
+				new spil.Field(9, new Color(0xFF9900), new Color(0x155B00), GameText.field9Text[0], GameText.field9Text[1], GameText.field9Text[2], 60),
+				new spil.Field(10, new Color(0xE2E2E2), new Color(0xE00000), GameText.field10Text[0], GameText.field10Text[1], GameText.field10Text[2], -80),
+				new spil.Field(11, new Color(0xC61300), new Color(0xFF7575), GameText.field11Text[0], GameText.field11Text[1], GameText.field11Text[2], -50),
+				new spil.Field(12, new Color(0xFFD700), new Color(0xCC7000), GameText.field12Text[0], GameText.field12Text[1], GameText.field12Text[2], 650), };
 
-		};
+		Field[] guiFields = {
+				new Street.Builder().setBgColor(fields[0].getBgColor()).setFgColor(fields[0].getFgColor()).setTitle(fields[0].getTitle())
+						.setSubText(fields[0].getSubText()).setDescription(fields[0].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[1].getBgColor()).setFgColor(fields[1].getFgColor()).setTitle(fields[1].getTitle())
+						.setSubText(fields[1].getSubText()).setDescription(fields[1].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[2].getBgColor()).setFgColor(fields[2].getFgColor()).setTitle(fields[2].getTitle())
+						.setSubText(fields[2].getSubText()).setDescription(fields[2].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[3].getBgColor()).setFgColor(fields[3].getFgColor()).setTitle(fields[3].getTitle())
+						.setSubText(fields[3].getSubText()).setDescription(fields[3].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[4].getBgColor()).setFgColor(fields[4].getFgColor()).setTitle(fields[4].getTitle())
+						.setSubText(fields[4].getSubText()).setDescription(fields[4].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[5].getBgColor()).setFgColor(fields[5].getFgColor()).setTitle(fields[5].getTitle())
+						.setSubText(fields[5].getSubText()).setDescription(fields[5].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[6].getBgColor()).setFgColor(fields[6].getFgColor()).setTitle(fields[6].getTitle())
+						.setSubText(fields[6].getSubText()).setDescription(fields[6].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[7].getBgColor()).setFgColor(fields[7].getFgColor()).setTitle(fields[7].getTitle())
+						.setSubText(fields[7].getSubText()).setDescription(fields[7].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[8].getBgColor()).setFgColor(fields[8].getFgColor()).setTitle(fields[8].getTitle())
+						.setSubText(fields[8].getSubText()).setDescription(fields[8].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[9].getBgColor()).setFgColor(fields[9].getFgColor()).setTitle(fields[9].getTitle())
+						.setSubText(fields[9].getSubText()).setDescription(fields[9].getDesc()).build(),
+				new Street.Builder().setBgColor(fields[10].getBgColor()).setFgColor(fields[10].getFgColor()).setTitle(fields[10].getTitle())
+						.setSubText(fields[10].getSubText()).setDescription(fields[10].getDesc()).build(), };
 
-		GUI.create(felter);
+		GUI.create(guiFields);
 
 		GUI.addPlayer("Player 1", 1000);
 		GUI.addPlayer("Player 2", 1000);

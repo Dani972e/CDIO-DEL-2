ce71c1bee41ab35d225776a3e4c49ad00e6b235e
Anusi <Anusi@DESKTOP-BG1PO0F>
2016-10-27 14:06:50 +0200

    Writing comments in Account class

--- 40_del2/src/spil/models/Account.java
+++ 40_del2/src/spil/models/Account.java
@@ -10,11 +10,26 @@ package spil.models;
 
 public class Account {
 
+	/**
+	 * Indkapslede klasse variabler, "fields."
+	 * 
+	 * @param MAX_COIN_AMOUNT    Maksimal værdi af variable coins.
+	 * @param DEFAUL_COIN_AMOUNT Startværdien af variable coins.
+	 * @param MIN_COIN_AMOUNT    Minimumsværdien af variable coins.
+	 * @param coins 		     Antallet af mønter som kontoen har lige nu.  
+	 */
 	private final int MAX_COIN_AMOUNT;
 	private final int DEFAULT_COIN_AMOUNT;
 	private final int MIN_COIN_AMOUNT;
 	private int coins;
 
+	/**
+	 * Constructor af Account klassen, som har tre varible inde i parameterlisten. 
+	 * 
+	 * @param maxCoinAmount 
+	 * @param defaultCoinAmount
+	 * @param minCoinAmount
+	 */
 	public Account(int maxCoinAmount, int defaultCoinAmount, int minCoinAmount) {
 		this.MAX_COIN_AMOUNT = maxCoinAmount;
 		this.DEFAULT_COIN_AMOUNT = defaultCoinAmount;
@@ -22,14 +37,28 @@ public class Account {
 		coins = DEFAULT_COIN_AMOUNT;
 	}
 
+	/**
+	 * Metode som indsætter amount til variable coins.
+	 * 
+	 * @param amount Mængden der bliver tilsat variable coins.
+	 */
 	public void addCoins(int amount) {
 		coins += amount;
 	}
 
+	/**
+	 * Metode der nulstiller variablen coins til DEFAULT_COIN_AMOUNT.
+	 */
 	public void resetAccount() {
 		coins = DEFAULT_COIN_AMOUNT;
 	}
 
+	/**
+	 * Metode der returnerer en boolean om coins er større eller lig med MAX_COIN_AMOUNT.
+	 * 
+	 * @return true Hvis coins er større eller lig med MAX_COIN_AMOUNT.
+	 * @return false Hvis coins ikke er større eller lig med MAX_COIN_AMOUNT.
+	 */
 	public boolean isFull() {
 		if (coins >= MAX_COIN_AMOUNT) {
 			return true;
@@ -37,6 +66,12 @@ public class Account {
 		return false;
 	}
 
+	/**
+	 * Metode der returnerer en boolean om coins er mindre eller lig med MIN_COIN_AMOUNT. 
+	 * 
+	 * @return true hvis coins er mindre eller lig med MIN_COIN_AMOUNT.
+	 * @return false hvis coins ikke er mindre eller lig med MIN_COIN_AMOUNT. 
+	 */
 	public boolean isEmpty() {
 		if (coins <= MIN_COIN_AMOUNT) {
 			return true;
@@ -44,6 +79,11 @@ public class Account {
 		return false;
 	}
 
+	/**
+	 * En getter metode der returnerer værdien af coins. 
+	 *
+	 * @return coins
+	 */
 	public int getCoins() {
 		return coins;
 	}

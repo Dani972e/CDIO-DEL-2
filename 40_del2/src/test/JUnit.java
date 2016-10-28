f0f468178bf9a3c2309c4a1644e6a0e0b6ddb704
Peter <peterxp@hotmail.fr>
2016-10-28 13:45:04 +0200

    Adding Test void

--- 40_del2/src/test/JUnit.java
+++ 40_del2/src/test/JUnit.java
@@ -1,10 +1,14 @@
 package test;
 
+import spil.models.*;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 
 /**
  * @author Daniel Larsen (s151641)
@@ -15,18 +19,60 @@ import org.junit.Test;
  */
 
 public class JUnit {
+	
+	static Player player1;
+	static Player player2;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		player1= new Player("Player 1");
+		player2= new Player("Player 2");
+		
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		player1=null;
+		player2=null;
 	}
 
 	@Test
-	public final void test() {
-		fail("Not yet implemented");
+	public final void testAddCoins() {
+		
+		int expected = 1500;
+		
+		player1.addCoins(500);
+		
+		assertEquals(expected, player1.getCoins());
+		
+		expected = 500;
+		
+		player1.addCoins(-1000);
+		
+		assertEquals(expected, player1.getCoins());
+	}
+	
+	@Test
+	public final void testOverflow(){
+		int expected = 2147483647;
+		
+		player1.addCoins(-player1.getCoins());
+		player1.addCoins(Integer.MAX_VALUE+1);
+		
+		assertEquals(expected,player1.getCoins());
+	}
+	
+	@Test
+	public final void testUnderflow(){
+		int expected = -2147483648;
+		
+		player1.addCoins(-player1.getCoins());
+		player1.addCoins(Integer.MIN_VALUE-1);
+		
+		assertEquals(expected,player1.getCoins());
 	}
 
 }

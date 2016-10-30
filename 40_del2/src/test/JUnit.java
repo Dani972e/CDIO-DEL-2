a8fd2b8e5339033472bdc2f0646aab8ecd2c15e2
Iyyel <Iyyelsec@hotmail.com>
2016-10-30 13:11:05 +0100

    JUnit test case formatting fixed.

--- 40_del2/src/test/JUnit.java
+++ 40_del2/src/test/JUnit.java
@@ -1,14 +1,12 @@
 package test;
 
-import spil.models.*;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import spil.models.Player;
 
 /**
  * @author Daniel Larsen (s151641)
@@ -19,61 +17,56 @@ import org.junit.Test;
  */
 
 public class JUnit {
-	
-	static Player player1;
-	static Player player2;
 
+	private static Player player1;
+	private static Player player2;
+	
 	@Before
 	public void setUp() throws Exception {
-		
-		player1= new Player("Player 1");
-		player2= new Player("Player 2");
-		
-		
+		player1 = new Player("Player 1");
+		player2 = new Player("Player 2");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		
-		player1=null;
-		player2=null;
+		player1 = null;
+		player2 = null;
 	}
 
 	@Test
 	public final void testAddCoins() {
-		
 		int expected = 1500;
-		
+
 		player1.addCoins(500);
-		
+
 		assertEquals(expected, player1.getCoins());
-		
+
 		expected = 500;
-		
+
 		player1.addCoins(-1000);
-		
+
 		assertEquals(expected, player1.getCoins());
-	}
-	
-	@Test
-	public final void testOverflow(){
-		int expected = 2147483647;
-		
-		
-		player1.addCoins(-player1.getCoins());
-		player1.addCoins(Integer.MAX_VALUE+1);
-		
-		assertEquals(expected,player1.getCoins());
-	}
-	
-	@Test
-	public final void testUnderflow(){
-		int expected = -2147483648;
-		
-		player1.addCoins(-player1.getCoins());
-		player1.addCoins(Integer.MIN_VALUE-1);
-		
-		assertEquals(expected,player1.getCoins());
 	}
 
+	@Test
+	public final void testAddCoinsOverflow() {
+
+		int expected = 2147483647;
+
+		player1.addCoins(-player1.getCoins());
+		player1.addCoins(Integer.MAX_VALUE + 1);
+
+		assertEquals(expected, player1.getCoins());
+	}
+
+	@Test
+	public final void testAddCoinsUnderflow() {
+		int expected = -2147483648;
+
+		player1.addCoins(-player1.getCoins());
+		player1.addCoins(Integer.MIN_VALUE - 1);
+
+		assertEquals(expected, player1.getCoins());
+	}
+	
 }

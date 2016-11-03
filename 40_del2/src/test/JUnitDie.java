4958f709761ec42506e33fcfbfb00c587fcb8927
Peter <peterxp@hotmail.fr>
2016-11-03 11:21:54 +0100

    Adding testDoubleRoll

--- 40_del2/src/test/JUnitDie.java
+++ 40_del2/src/test/JUnitDie.java
@@ -11,19 +11,22 @@ import spil.models.Die;
 public class JUnitDie {
 
 	private Die die1;
+	private Die die2;
 
 	@Before
 	public void setUp() throws Exception {
 		die1 = new Die(6);
+		die2 = new Die(6);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		die1 = null;
+		die2= null;
 	}
 
 	@Test
-	public void testRoll() {
+	public void testRoll10() {
 		die1.setMaxFaceValue(10);
 
 		int iterations = 1000000;
@@ -70,12 +73,81 @@ public class JUnitDie {
 			}
 
 		}
-
+		System.out.println("-------------------");
 		for (int i = 0; i < 10; i++) {
 			System.out.println((i + 1) + ": " + rollArray[i]);
 		}
 
 		int other = rollArray[10];
+		System.out.println("other: " + other);
+
+		int expected = 0;
+
+		assertEquals(expected, other);
+	}
+	
+	//Til 2 Dice med MAX_FACE_VALUE 6
+	@Test
+	public void testDoubleRoll() {
+		
+		die1.setMaxFaceValue(6);
+		
+		die2.setMaxFaceValue(6);
+		
+		int iterations = 1000000;
+
+		int[] rollArray = new int[11];
+
+		for (int i = 0; i < iterations; i++) {
+			int roll = die1.roll()+die2.roll();
+
+			switch (roll) {
+			case 2:
+				rollArray[0]++;
+				break;
+			case 3:
+				rollArray[1]++;
+				break;
+			case 4:
+				rollArray[2]++;
+				break;
+			case 5:
+				rollArray[3]++;
+				break;
+			case 6:
+				rollArray[4]++;
+				break;
+			case 7:
+				rollArray[5]++;
+				break;
+			case 8:
+				rollArray[6]++;
+				break;
+			case 9:
+				rollArray[7]++;
+				break;
+			case 10:
+				rollArray[8]++;
+				break;
+			case 11:
+				rollArray[9]++;
+				break;
+			case 12:
+				rollArray[10]++;
+				break;
+			default:
+				rollArray[11]++;
+				break;
+			}
+
+		}
+		System.out.println("-------------------");
+		
+		for (int i = 0; i < 11; i++) {
+			System.out.println((i+2) + ": " + rollArray[i]);
+		}
+		
+		int other = 0;
 		System.out.println("other: " + other);
 
 		int expected = 0;
